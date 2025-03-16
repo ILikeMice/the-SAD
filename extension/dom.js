39ffb35d32a5a72d919ab226ba3a47089a237579
ShyMike <122023566+ImShyMike@users.noreply.github.com>
2025-03-16 10:34:17 +0000

    ffs

--- extension/dom.js
+++ extension/dom.js
@@ -101,9 +101,9 @@
 
   // extension/annoyances/distance.ts
   function distance(distance2) {
-    const size = Math.min(Math.max(distance2, 2), 75);
+    const size = Math.min(Math.max(distance2, 2), 75) / 75;
     console.log("Using size:", size, "from distance:", distance2);
-    document.body.style.transform = "scale(" + globalThis.screen.availHeight / (size / 4) + ")";
+    document.body.style.fontSize = `${size}em`;
   }
 
   // extension/dom.ts
