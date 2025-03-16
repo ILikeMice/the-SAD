f56175dfefefe525ac24eb45e74d595f3bfcd852
Ayo Reis <hey@ayoreis.com>
2025-03-16 09:25:11 +0000

    mostly env, and more

--- extension/popup.js
+++ extension/popup.js
@@ -2,7 +2,6 @@
   // extension/popup.ts
   var crank_meter = document.getElementById("crank");
   var humidity_meter = document.getElementById("humidity");
-  var distance_meter = document.getElementById("distance");
   chrome.runtime.onMessage.addListener((data) => {
     const message = JSON.parse(data);
     console.log(message);
@@ -11,9 +10,6 @@
     }
     if (message.type === "humidity") {
       humidity_meter.value = message.value;
-    }
-    if (message.type === "distance") {
-      distance_meter.value = message.value;
     }
   });
 })();
