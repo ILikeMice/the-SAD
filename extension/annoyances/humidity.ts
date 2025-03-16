17f04c818f99442f48173b696f54ffedbda7929f
ShyMike <122023566+ImShyMike@users.noreply.github.com>
2025-03-16 10:46:30 +0000

    remove humidity

--- extension/annoyances/humidity.ts
+++ extension/annoyances/humidity.ts
@@ -1,25 +1,26 @@
-const tracked = [];
-let freezing = false;
+// const tracked = [];
+// let freezing = false;
 
 export function humidity(humidity: number) {
-	if (humidity < 55 || freezing) { return; }
-	freezing = true;
-	const element = getRandomElement();
-	tracked.push(element);
-	let i = 0;
-	const id = setInterval(() => {
-		i++;
-		element.style.filter = `blur(${i * 3}px)`;
-		if (i >= 10) {
-			clearInterval(id);
-			freezing = false;
-			console.log("Unfroze");
-		}
-	}, 300);
+	console.log("Humidity:", humidity);
+	// if (humidity < 55 || freezing) { return; /}
+	// freezing = true;
+	// const element = getRandomElement();
+	// tracked.push(element);
+	// let i = 0;
+	// const id = setInterval(() => {
+	// 	i++;
+	// 	element.style.filter = `blur(${i * 3}px)`;
+	// 	if (i >= 10) {
+	// 		clearInterval(id);
+	// 		freezing = false;
+	// 		console.log("Unfroze");
+	// 	}
+	// }, 300);
 }
 
-function getRandomElement() {
-	const elements = document.body.querySelectorAll("*");
-	const randomElement = elements[Math.floor(Math.random() * elements.length)];
-	return randomElement as HTMLElement;
-}
+// function getRandomElement() {
+// 	const elements = document.body.querySelectorAll("*");
+// 	const randomElement = elements[Math.floor(Math.random() * elements.length)];
+// 	return randomElement as HTMLElement;
+// }
