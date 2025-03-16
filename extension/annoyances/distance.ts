e9150a19f7bc5a482d925094ec20e5cb133bd327
ShyMike <122023566+ImShyMike@users.noreply.github.com>
2025-03-16 08:53:16 +0000

    still testing in prod

--- extension/annoyances/distance.ts
+++ extension/annoyances/distance.ts
@@ -1,5 +1,5 @@
 export function distance(distance: number) {
 	const size = Math.min(Math.max(distance, 2), 75) / 75 * 100;
 	console.log("Using size:", size, "from distance:", distance);
-	document.body.style.fontSize = `${size}%`;
+	document.body.style.fontSize = `${size/2}px`;
 }
